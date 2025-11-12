c6d838f7e00dc7a84cb070858db35a1350bc47d3
AndreMD287 <bastianlanfor@gmail.com>
2025-11-11 21:05:07 -0500

    feat: 4to workshop: deploy de contrato en testnet usando Soroban y Rust

---
+++ contracts/rent-a-car/src/contract.rs
@@ -0,0 +1,23 @@
+use crate::interfaces::contract::RentACarContractTrait;
+use soroban_sdk::{contract, contractimpl, symbol_short, Address, Env, Symbol};
+
+#[contract]
+pub struct RentACarContract;
+
+pub const ADMIN_KEY: &Symbol = &symbol_short!("ADMIN");
+pub const TOKEN_KEY: &Symbol = &symbol_short!("TOKEN");
+
+#[contractimpl]
+impl RentACarContractTrait for RentACarContract {
+    fn __constructor(env: &Env, admin: Address, token: Address) {
+        env.storage().instance().set(ADMIN_KEY, &admin);
+        env.storage().instance().set(TOKEN_KEY, &token);
+    }
+
+    fn get_admin(env: &Env) -> Address {
+        env.storage()
+            .instance()
+            .get::<Symbol, Address>(ADMIN_KEY)
+            .unwrap()
+    }
+}
---
+++ contracts/rent-a-car/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+mod contract;
+pub mod interfaces;
+mod tests;
+
+pub use contract::RentACarContract;
